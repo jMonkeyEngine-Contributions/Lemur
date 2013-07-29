d0f2c1b62dbacc5c921a48c1a3f78e1d562feb1d
PSpeed42@gmail.com <PSpeed42@gmail.com@d35518cc-fea4-478f-9a4a-bf00402cdad9>
2013-07-29 06:54:32 +0000

    Added an InsetsComponent subclass that dynamically sets the insets based on the current amount of "fill", ie: the delta between actual size and preferred size. BasicDemo has been modified to show this behavior.

---
+++ src/com/simsilica/lemur/component/DynamicInsetsComponent.java
@@ -0,0 +1,139 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012-2012 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.simsilica.lemur.component;
+
+import com.jme3.math.Vector3f;
+import com.simsilica.lemur.Insets3f;
+
+
+/**
+ *
+ *  @author    Paul Speed
+ */
+public class DynamicInsetsComponent extends InsetsComponent {
+
+    private Vector3f lastPreferredSize;
+
+    public DynamicInsetsComponent( float top, float left, float bottom, float right ) {
+        super( new Insets3f(top, left, bottom, right) );
+    }
+
+    public DynamicInsetsComponent( float top, float left, float bottom, float right,
+                            float front, float back ) {
+        super( new Insets3f(top, left, bottom, right, front, back) );
+    }
+
+    public DynamicInsetsComponent( Insets3f insets ) {
+        super( insets );
+    }
+
+    @Override
+    public void setInsets( Insets3f insets ) {
+
+        Insets3f balanced = insets.clone();
+
+        // Make sure the insets add to 1.0 in each axis
+        for( int i = 0; i < 3; i++ ) {
+            float min = balanced.min.get(i);
+            float max = balanced.max.get(i);
+            if( min == 0 && max == 0 ) {
+                // If both are 0 then center... prevents divide by zero
+                min = 0.5f;
+                max = 0.5f;
+            }
+            float size = min + max;
+            float scale = 1 / size;
+            balanced.min.set(i, min * scale);
+            balanced.max.set(i, max * scale);
+        }
+
+        super.setInsets( balanced );
+    }
+
+    @Override
+    public DynamicInsetsComponent clone() {
+        DynamicInsetsComponent result = (DynamicInsetsComponent)super.clone();
+        result.lastPreferredSize = null;
+        return result;
+    }
+
+    @Override
+    public void calculatePreferredSize( Vector3f size ) {
+
+        // Keep track of the preferred size of the rest of
+        // the stack up to this point.  We don't add any insets
+        // here.
+        lastPreferredSize = size.clone();
+    }
+
+    @Override
+    public void reshape( Vector3f pos, Vector3f size ) {
+
+        if( lastPreferredSize == null ) {
+            // There is nothing we can do, so we won't do anything
+            return;
+        }
+
+        // Otherwise, see what the difference is between our
+        // desired size and
+        Vector3f delta = size.subtract(lastPreferredSize);
+        Insets3f insets = getInsets();
+
+        for( int i = 0; i < 3; i++ ) {
+            float d = delta.get(i);
+            if( d <= 0 ) {
+                // We don't go smaller than preferred size so
+                // we skip if less than 0 and 0 would be a no-op
+                // anyway so we skip then too
+                continue;
+            }
+
+            float min = insets.min.get(i);
+            float p = pos.get(i);
+            float s = size.get(i);
+
+            if( i == 1 ) {
+                // To match regular insets we invert y adjustment
+                // so that min is at the top.
+                pos.set(i, p - min * d);
+            } else {
+                pos.set(i, p + min * d);
+            }
+            size.set(i, s - d);
+        }
+    }
+
+}
+
--- src/com/simsilica/lemur/demo/BasicDemo.java
+++ src/com/simsilica/lemur/demo/BasicDemo.java
@@ -50,8 +50,10 @@ import com.simsilica.lemur.Label;
 import com.simsilica.lemur.LayerComparator;
 import com.simsilica.lemur.Panel;
 import com.simsilica.lemur.Slider;
+import com.simsilica.lemur.component.DynamicInsetsComponent;
 import com.simsilica.lemur.component.QuadBackgroundComponent;
 import com.simsilica.lemur.component.TbtQuadBackgroundComponent;
+import com.simsilica.lemur.core.GuiControl;
 import com.simsilica.lemur.core.VersionedReference;
 import com.simsilica.lemur.event.DragHandler;
 import com.simsilica.lemur.event.MouseEventControl;
@@ -65,74 +67,74 @@ import com.simsilica.lemur.style.Styles;
  *  @author    Paul Speed
  */
 public class BasicDemo extends SimpleApplication {
- 
-    // Define some model references we will use in 
-    // update. 
-    private VersionedReference<Double> redRef;      
-    private VersionedReference<Double> greenRef;      
-    private VersionedReference<Double> blueRef;                  
-    private VersionedReference<Double> alphaRef;          
-    private VersionedReference<Boolean> showStatsRef; 
-    private VersionedReference<Boolean> showFpsRef; 
- 
-    private ColorRGBA boxColor = ColorRGBA.Blue.clone();   
-    
+
+    // Define some model references we will use in
+    // update.
+    private VersionedReference<Double> redRef;
+    private VersionedReference<Double> greenRef;
+    private VersionedReference<Double> blueRef;
+    private VersionedReference<Double> alphaRef;
+    private VersionedReference<Boolean> showStatsRef;
+    private VersionedReference<Boolean> showFpsRef;
+
+    private ColorRGBA boxColor = ColorRGBA.Blue.clone();
+
     public static void main(String[] args) {
         BasicDemo app = new BasicDemo();
         app.start();
-    }    
+    }
 
     @Override
     public void simpleInitApp() {
-        
+
         // Initialize the globals access so that the defualt
-        // components can find what they need.       
+        // components can find what they need.
         GuiGlobals.initialize(this);
-        
+
         // Remove the flycam because we don't want it in this
         // demo
-        stateManager.detach( stateManager.getState(FlyCamAppState.class) );    
- 
+        stateManager.detach( stateManager.getState(FlyCamAppState.class) );
+
         // Now, let's create some styles in code.
         // For this demo, we'll just give some of the elements
         // different backgrounds as we define a "glass" style.
         // We also define a custom element type called "spacer" which
         // picks up a specific style.
-        Styles styles = GuiGlobals.getInstance().getStyles();        
+        Styles styles = GuiGlobals.getInstance().getStyles();
         styles.getSelector( Slider.THUMB_ID, "glass" ).set( "text", "[]", false );
-        styles.getSelector( Panel.ELEMENT_ID, "glass" ).set( "background", 
-                                new QuadBackgroundComponent(new ColorRGBA(0, 0.25f, 0.25f, 0.5f)) ); 
-        styles.getSelector( Checkbox.ELEMENT_ID, "glass" ).set( "background", 
-                                new QuadBackgroundComponent(new ColorRGBA(0, 0.5f, 0.5f, 0.5f)) ); 
-        styles.getSelector( "spacer", "glass" ).set( "background", 
-                                new QuadBackgroundComponent(new ColorRGBA(1, 0.0f, 0.0f, 0.0f)) );  
-        styles.getSelector( "header", "glass" ).set( "background", 
-                                new QuadBackgroundComponent(new ColorRGBA(0, 0.75f, 0.75f, 0.5f)) );  
+        styles.getSelector( Panel.ELEMENT_ID, "glass" ).set( "background",
+                                new QuadBackgroundComponent(new ColorRGBA(0, 0.25f, 0.25f, 0.5f)) );
+        styles.getSelector( Checkbox.ELEMENT_ID, "glass" ).set( "background",
+                                new QuadBackgroundComponent(new ColorRGBA(0, 0.5f, 0.5f, 0.5f)) );
+        styles.getSelector( "spacer", "glass" ).set( "background",
+                                new QuadBackgroundComponent(new ColorRGBA(1, 0.0f, 0.0f, 0.0f)) );
+        styles.getSelector( "header", "glass" ).set( "background",
+                                new QuadBackgroundComponent(new ColorRGBA(0, 0.75f, 0.75f, 0.5f)) );
 
         // Now construct some HUD panels in the "glass" style that
         // we just configured above.
         Container hudPanel = new Container("glass");
         hudPanel.setLocalTranslation( 5, cam.getHeight() - 50, 0 );
         guiNode.attachChild(hudPanel);
- 
+
         // Create a top panel for some stats toggles.
         Container panel = new Container("glass");
         hudPanel.addChild(panel);
-        
+
         panel.setBackground(new QuadBackgroundComponent(new ColorRGBA(0,0.5f,0.5f,0.5f),5,5, 0.02f, false));
         panel.addChild( new Label( "Stats Settings", new ElementId("header"), "glass" ) );
         panel.addChild( new Panel( 2, 2, ColorRGBA.Cyan, "glass" ) ).setUserData( LayerComparator.LAYER, 2 );
-        
+
         // Adding components returns the component so we can set other things
         // if we want.
         Checkbox temp = panel.addChild( new Checkbox( "Show Stats" ) );
-        temp.setChecked(true); 
+        temp.setChecked(true);
         showStatsRef = temp.getModel().createReference();
-        
+
         temp = panel.addChild( new Checkbox( "Show FPS" ) );
-        temp.setChecked(true); 
+        temp.setChecked(true);
         showFpsRef = temp.getModel().createReference();
-        
+
 
         // Custom "spacer" element type
         hudPanel.addChild( new Panel( 10f, 10f, new ElementId("spacer"), "glass" ) );
@@ -152,19 +154,19 @@ public class BasicDemo extends SimpleApplication {
         blueRef = panel.addChild( new Slider(new DefaultRangedValueModel(0,100,100), "glass") ).getModel().createReference();
         panel.addChild( new Label( "Alpha:" ) );
         alphaRef = panel.addChild( new Slider(new DefaultRangedValueModel(0,100,100), "glass") ).getModel().createReference();
-        
-        hudPanel.addChild(panel);       
+
+        hudPanel.addChild(panel);
         guiNode.attachChild(hudPanel);
-                
+
         // Increase the default size of the hud to be a little wider
         // if it would otherwise be smaller.  Height is unaffected.
         Vector3f hudSize = new Vector3f(200,0,0);
         hudSize.maxLocal(hudPanel.getPreferredSize());
-        hudPanel.setPreferredSize( hudSize );  
-        
+        hudPanel.setPreferredSize( hudSize );
+
         // Note: after next nightly, this will also work:
-        // hudPanel.setPreferredSize( new Vector3f(200,0,0).maxLocal(hudPanel.getPreferredSize()) );         
-        
+        // hudPanel.setPreferredSize( new Vector3f(200,0,0).maxLocal(hudPanel.getPreferredSize()) );
+
         // Something in scene
         Box box = new Box( Vector3f.ZERO, 1, 1, 1 );
         Geometry geom = new Geometry( "Box", box );
@@ -173,21 +175,24 @@ public class BasicDemo extends SimpleApplication {
         mat.getAdditionalRenderState().setBlendMode( BlendMode.Alpha );
         geom.setMaterial(mat);
         rootNode.attachChild( geom );
-        
+
         // A draggable bordered panel
         Container testPanel = new Container();
         testPanel.setPreferredSize( new Vector3f(200, 200,0) );
         testPanel.setBackground( TbtQuadBackgroundComponent.create( "/com/simsilica/lemur/icons/border.png",
                                                                     1, 2, 2, 3, 3, 0, false ) );
-        testPanel.addChild( new Label( "Border Test" ) );
+        Label test = testPanel.addChild( new Label( "Border Test" ) );
+
+        // Center the text in the box.
+        test.setInsetsComponent(new DynamicInsetsComponent(0.5f, 0.5f, 0.5f, 0.5f));
         testPanel.setLocalTranslation( 400, 400, 0 );
-        
-        testPanel.addControl( new MouseEventControl(new DragHandler()) );                                                                    
-        guiNode.attachChild( testPanel );        
+
+        testPanel.addControl( new MouseEventControl(new DragHandler()) );
+        guiNode.attachChild( testPanel );
     }
-    
+
     @Override
-    public void simpleUpdate(float tpf) 
+    public void simpleUpdate(float tpf)
     {
         if( showStatsRef.update() )
             {
@@ -197,7 +202,7 @@ public class BasicDemo extends SimpleApplication {
             {
             setDisplayFps( showFpsRef.get() );
             }
-            
+
         boolean updateColor = false;
         if( redRef.update() )
             updateColor = true;
@@ -209,10 +214,10 @@ public class BasicDemo extends SimpleApplication {
             updateColor = true;
         if( updateColor )
             {
-            boxColor.set( (float)(redRef.get()/100.0), 
-                          (float)(greenRef.get()/100.0), 
-                          (float)(blueRef.get()/100.0),                  
+            boxColor.set( (float)(redRef.get()/100.0),
+                          (float)(greenRef.get()/100.0),
+                          (float)(blueRef.get()/100.0),
                           (float)(alphaRef.get()/100.0) );
-            }                  
+            }
     }
 }
