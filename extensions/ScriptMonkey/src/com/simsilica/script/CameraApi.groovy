f6a514fb2fbbcd8060e229075d3a233538c8ee02
PSpeed42@gmail.com <PSpeed42@gmail.com@d35518cc-fea4-478f-9a4a-bf00402cdad9>
2013-12-01 01:15:52 +0000

    Added a "go" command for moving the camera.

--- extensions/ScriptMonkey/src/com/simsilica/script/CameraApi.groovy
+++ extensions/ScriptMonkey/src/com/simsilica/script/CameraApi.groovy
@@ -14,3 +14,13 @@ Quaternion look( Facing facing ) {
     camera.setRotation(facing.getRotation());
     return facing.getRotation();
 }
+
+Vector3f go( Number x, Number y, Number z ) {
+    Vector3f v = vec3(x,y,z);
+    camera.setLocation(v);
+    return v;
+}
+
+Vector3f go( Vector3f loc ) {
+    go(loc.x, loc.y, loc.z);
+}
