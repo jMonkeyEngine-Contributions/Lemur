8138d101bd22e5ff89038df4f4e432ee93f9a485
PSpeed42@gmail.com <PSpeed42@gmail.com@d35518cc-fea4-478f-9a4a-bf00402cdad9>
2013-12-02 07:26:16 +0000

    Added colorrgba support to math package.

--- extensions/ScriptMonkey/src/com/simsilica/script/MathApi.groovy
+++ extensions/ScriptMonkey/src/com/simsilica/script/MathApi.groovy
@@ -91,6 +91,14 @@ Vector3f.metaClass {
     
     getYz {
         return new Vector2f(delegate.y, delegate.z);
+    }
+    
+    asType { Class type ->
+        if( type == ColorRGBA ) {
+            return new ColorRGBA(delegate.x, delegate.y, delegate.z, 1);
+        } else {
+            throw new org.codehaus.groovy.runtime.typehandling.GroovyCastException(delegate, type);
+        }
     }    
 }
 
@@ -195,7 +203,91 @@ Vector4f.metaClass {
     // vec3 versions       
     getXyz {
         return new Vector3f(delegate.x, delegate.y, delegate.z);
+    }
+        
+    asType( ColorRGBA ) {
+        return new ColorRGBA(delegate.x, delegate.y, delegate.z, delegate.w);
     }    
 }
 
+ColorRGBA color( Number r, Number g, Number b, Number a ) {
+    return new ColorRGBA(r.floatValue(), g.floatValue(), b.floatValue(), a.floatValue());
+}
 
+ColorRGBA color( Number r, Number g, Number b ) {
+    return color(r, g, b, 1);
+}  
+
+ColorRGBA color( Vector3f v ) {
+    return color(v.x, v.y, v.z, 1);
+}
+
+ColorRGBA color( Vector4f v ) {
+    return color(v.x, v.y, v.z, v.w);
+}
+
+ColorRGBA.metaClass {
+
+    plus { ColorRGBA v ->
+        return delegate.add(v);
+    }
+    
+    minus { ColorRGBA v ->
+        return delegate.subtract(v);
+    }
+    
+    multiply { Number scale ->
+        return delegate.mult(scale.floatValue());
+    }
+    
+    div { Number scale ->
+        return delegate.divide(scale.floatValue());
+    }
+    
+    getAt { int i ->
+        switch( i ) {
+            case 0:
+                return delegate.r;
+            case 1:
+                return delegate.g;
+            case 2:
+                return delegate.b;
+            case 3:
+                return delegate.a;
+            default:
+                throw new IndexOutOfBoundsException(i);
+        }
+    }
+    
+    putAt { int i, Number val ->
+        switch( i ) {
+            case 0:
+                delegate.r = val.floatValue();
+                break;
+            case 1:
+                delegate.g = val.floatValue();
+                break;
+            case 2:
+                delegate.b = val.floatValue();
+                break;
+            case 3:
+                delegate.a = val.floatValue();
+                break;
+            default:
+                throw new IndexOutOfBoundsException(i);
+        }
+        return val;
+    }
+    
+    // I can't get dynamic swizzling with propertyMissing to work
+    // like I want because I can't figure out how to delegate to
+    // the original behavior if the swizzle misses.  So I will hard-code
+    // a few.
+    getRgb {
+        return new Vector3f(delegate.r, delegate.g, delegate.b);
+    }    
+
+    asType( Vector4f ) {
+        return new Vector4f(delegate.r, delegate.g, delegate.b, delegate.a);
+    }
+}
