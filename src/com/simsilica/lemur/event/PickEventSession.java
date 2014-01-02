72fdddac1179a1d7663d40b89d7d0546eb2cb00c
PSpeed42@gmail.com <PSpeed42@gmail.com@d35518cc-fea4-478f-9a4a-bf00402cdad9>
2014-01-02 18:59:34 +0000

    Fixed a clone state bug where the root list wouldn't be regenerated properly in the clones.

--- src/com/simsilica/lemur/event/PickEventSession.java
+++ src/com/simsilica/lemur/event/PickEventSession.java
@@ -93,6 +93,7 @@ public class PickEventSession {
     
     protected PickEventSession( Map<Collidable, RootEntry> roots ) {
         this.roots.putAll(roots);
+        this.rootList = null;
     } 
  
     /**
