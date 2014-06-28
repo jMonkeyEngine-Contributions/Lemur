2debd16859bc4850cf764535c2931d31199d73a5
PSpeed42@gmail.com <PSpeed42@gmail.com@d35518cc-fea4-478f-9a4a-bf00402cdad9>
2014-06-28 04:10:52 +0000

    Expose the icon base to subclasses just in case.

--- src/com/simsilica/lemur/GuiGlobals.java
+++ src/com/simsilica/lemur/GuiGlobals.java
@@ -163,6 +163,10 @@ public class GuiGlobals {
     protected AssetManager getAssetManager() {
         return assets;
     }
+ 
+    protected String getIconBase() {
+        return iconBase;
+    }
 
     protected void logBuildInfo() {
         try {
