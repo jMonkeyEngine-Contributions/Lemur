f10ad3755b9294781e48c6a27b2c957994ea7f38
PSpeed42@gmail.com <PSpeed42@gmail.com@d35518cc-fea4-478f-9a4a-bf00402cdad9>
2014-12-02 06:11:33 +0000

    Added Action and ActionButton.  Action is similar to Swing's Action and ActionButton is a Button specialization that uses Action.

---
+++ extensions/LemurProto/src/main/java/com/simsilica/lemur/Action.java
@@ -0,0 +1,212 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2014, Simsilica, LLC
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.simsilica.lemur;
+
+import com.google.common.base.Objects;
+import com.simsilica.lemur.core.GuiComponent;
+import com.simsilica.lemur.core.VersionedObject;
+import com.simsilica.lemur.core.VersionedReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *  A combination of a command and some attributes
+ *  that define what the GUI element should look like.
+ *  The action is versioned so that GUI elements can
+ *  automatically update themselves as these attributes
+ *  are changed.  This is similar to Swing's Action.
+ *
+ *  @author    Paul Speed
+ */
+public abstract class Action implements VersionedObject<Action>, Command<Button> {
+
+    public static final String KEY_ENABLED = "enabled";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_ICON = "icon";
+    public static final String KEY_LARGE_ICON = "largeIcon";
+    public static final String KEY_SHORT_DESCRIPTION = "shortDescription";
+    public static final String KEY_SELECTED = "selected";
+
+    private long version;
+    private Map<String, Object> properties = new HashMap<String, Object>();
+    private boolean enabled = true;
+    
+    public Action() {
+    }
+
+    public Action( String name ) {
+        setName(name);
+    }
+    
+    public Action( String name, GuiComponent icon ) {
+        setName(name);
+        setIcon(icon);
+    }
+
+    public Action( String name, GuiComponent icon, boolean enabled ) {
+        setName(name);
+        setIcon(icon);
+        setEnabled(enabled);
+    }
+
+    /**
+     *  Subclasses must override this to perform the action.
+     */
+    @Override
+    public abstract void execute( Button source );
+ 
+    /**
+     *  Sets the enabled/disabled state of this action.  Action 
+     *  GUI elements that support being disabled will adhere to
+     *  this state.
+     */
+    public void setEnabled( boolean b ) {
+        putValue(KEY_ENABLED, b);
+    }
+    
+    public boolean isEnabled() {
+        return getValue(KEY_ENABLED, true);
+    }
+
+    /**
+     *  Sets the selected/deselected state of this action.
+     *  Action GUI elements that support selection (checkboxes)
+     *  will adhere to this state and reflect it back into the
+     *  action itself.
+     */
+    public void setSelected( boolean b ) {
+        putValue(KEY_SELECTED, b);
+    }
+    
+    public boolean isSelected() {
+        return getValue(KEY_SELECTED, true);
+    }
+ 
+    /**
+     *  Sets the name of the action that will be used as the
+     *  label in wrapping Action GUI elements.
+     */
+    public void setName( String name ) {
+        putValue(KEY_NAME, name);
+    }
+    
+    public String getName() {
+        return getValue(KEY_NAME);
+    }
+    
+    /**
+     *  Sets the icon that will be used as the icon
+     *  in wrapping Action GUI elements.  This is used when
+     *  a smaller icon is needed or when there is no large
+     *  icon specified.
+     */
+    public void setIcon( GuiComponent component ) {
+        putValue(KEY_ICON, component);
+    }
+    
+    public GuiComponent getIcon() {
+        return getValue(KEY_ICON);
+    }
+
+    /**
+     *  Sets the icon that will be used as the icon
+     *  in wrapping Action GUI elements.  ActionButton will
+     *  use this as its icon if specified.  getLargeIcon()
+     *  defaults to getIcon() when not set.  
+     */
+    public void setLargeIcon( GuiComponent component ) {
+        putValue(KEY_LARGE_ICON, component);
+    }
+    
+    public GuiComponent getLargeIcon() {
+        return getValue(KEY_LARGE_ICON, getIcon());
+    }
+ 
+    /**
+     *  Sets a general value onto this action that other
+     *  action users or custom GUI elements can reference.
+     */
+    public void putValue( String key, Object value ) {
+        Object existing = properties.put(key, value);
+        if( !Objects.equal(existing, value) ) {
+            incrementVersion();
+        }        
+    }
+ 
+    /**
+     *  Returns a previously set value from this action or
+     *  null if the property has not been set.
+     */
+    public <T> T getValue( String key ) {
+        return (T)properties.get(key);
+    }
+
+    /**
+     *  Returns a previously set value from this action or
+     *  defaultValue if the property has not been set.
+     */
+    public <T> T getValue( String key, T defaultValue ) {
+        Object result = properties.get(key);
+        return result == null ? defaultValue : (T)result;
+    }
+ 
+    protected void incrementVersion() {
+        version++;
+    }
+    
+    @Override
+    public long getVersion() {
+        return version;
+    }
+
+    @Override
+    public Action getObject() {
+        return this;
+    }
+
+    @Override
+    public VersionedReference<Action> createReference() {
+        return new VersionedReference<Action>(this);
+    }
+    
+    @Override   
+    public String toString() {
+        return getClass().getName() + "[properties=" + properties + "]";
+    }
+}
---
+++ extensions/LemurProto/src/main/java/com/simsilica/lemur/ActionButton.java
@@ -0,0 +1,148 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2014, Simsilica, LLC
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.simsilica.lemur;
+
+import com.simsilica.lemur.core.GuiComponent;
+import com.simsilica.lemur.core.GuiControl;
+import com.simsilica.lemur.core.VersionedReference;
+import com.simsilica.lemur.style.ElementId;
+
+
+/**
+ *  A Button specialization that wraps an Action and
+ *  reflects changes to the Action's properties.
+ *
+ *  @author    Paul Speed
+ */
+public class ActionButton extends Button {
+ 
+    private static final String KEY_ICON = "icon";
+    
+    private Action action;
+    private VersionedReference<Action> actionRef;
+    
+    public ActionButton( Action action ) {
+        this(action, new ElementId(ELEMENT_ID), null);
+    }
+ 
+    public ActionButton( Action action, String style ) {
+        this(action, new ElementId(ELEMENT_ID), style);
+    }
+
+    public ActionButton( Action action, ElementId elementId, String style ) {
+        super(action.getName(), elementId, style);
+        setAction(action);
+        addClickCommands(new ClickCommand());
+    }
+ 
+    /**
+     *  Sets a new Action to this button and clears any previously
+     *  set action.  The button's text, icon, etc. will be changed
+     *  to the Action's properties and will be kept in sync as the
+     *  action changes.  Click events will be passed through to the
+     *  action.
+     */
+    public final void setAction( Action action ) {
+        if( this.action == action ) {
+            return;
+        }
+        this.action = action;
+        this.actionRef = action == null ? null : action.createReference();
+        updateButton();
+    }
+    
+    public Action getAction() {
+        return action;
+    }
+ 
+    protected void setIcon( GuiComponent c ) {
+        if( getControl(GuiControl.class).getComponent(KEY_ICON) != null ) {
+            // Remove the old one
+            getControl(GuiControl.class).removeComponent(KEY_ICON);
+        }
+
+        if( c != null ) {
+            GuiControl gui = getControl(GuiControl.class);
+            GuiComponent after = gui.getFirstComponent(KEY_BACKGROUND, KEY_INSETS);
+            int index = gui.getComponentIndex(after);
+            gui.addComponent(index+1, KEY_ICON, c);
+        }
+    }
+ 
+    @Override
+    public void updateLogicalState( float tpf ) {
+        super.updateLogicalState(tpf);
+        if( actionRef != null && actionRef.update() ) {
+            updateButton();
+        }
+    }
+    
+    protected void updateButton() {
+        updateText();
+        updateEnabled();
+        updateIcon();
+    }
+    
+    protected void updateText() {
+        setText(action != null ? action.getName() : null);
+    }
+    
+    protected void updateEnabled() {
+        setEnabled(action != null ? action.isEnabled() : false);
+    }
+    
+    protected void updateIcon() {
+        setIcon(action != null ? action.getLargeIcon() : null);
+    }
+    
+    /**
+     *  Registered with the parent class for all click events
+     *  which then delegates to the action.  We could have gone
+     *  direct but I wanted the intercept just in case.
+     */
+    private class ClickCommand implements Command<Button> {
+
+        @Override
+        public void execute( Button source ) {
+            if( action != null ) {
+                action.execute(source);
+            }      
+        }
+    } 
+}
+
+
