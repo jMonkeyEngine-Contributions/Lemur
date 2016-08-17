6e87ed49ea31f1f62ea1eec371d53c6bf0122255
Paul Speed <pspeed42@users.noreply.github.com>
2016-08-17 15:57:06 -0400

    Fixed ColorChooser's default constructor to actually do stuff... ie: delegated to the common constructor.

--- extensions/LemurProto/src/main/java/com/simsilica/lemur/ColorChooser.java
+++ extensions/LemurProto/src/main/java/com/simsilica/lemur/ColorChooser.java
@@ -1,36 +1,36 @@
 /*
  * $Id$
- * 
+ *
  * Copyright (c) 2015, Simsilica, LLC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions 
+ * modification, are permitted provided that the following conditions
  * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright 
+ *
+ * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
- *    the documentation and/or other materials provided with the 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
  *    distribution.
- * 
- * 3. Neither the name of the copyright holder nor the names of its 
- *    contributors may be used to endorse or promote products derived 
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -81,50 +81,51 @@ public class ColorChooser extends Panel {
                 raster.setPixel(i, j, toJmeColor(hsb));
             }
         }
-    }    
- 
+    }
+
     private VersionedObject<ColorRGBA> model;
     private VersionedReference<ColorRGBA> modelRef;
     private Texture2D swatchTexture;
- 
+
     private Panel value;
-    private Container colorPanel;   
+    private Container colorPanel;
     private Panel colors;
     private QuadBackgroundComponent valueColor = new QuadBackgroundComponent();
     private QuadBackgroundComponent swatchComponent;
     private Slider brightness;
     private VersionedReference brightnessRef;
-    
+
     private float hIndex = 0;
     private float sIndex = 0;
     private float bIndex = 0.5f;
-        
+
     public ColorChooser() {
+        this(true, null, new ElementId(ELEMENT_ID), null);
     }
 
     public ColorChooser( String style ) {
         this(true, null, new ElementId(ELEMENT_ID), style);
     }
- 
+
     public ColorChooser( ElementId elementId, String style ) {
         this(true, null, elementId, style);
     }
- 
+
     protected ColorChooser( boolean applyStyles, VersionedObject<ColorRGBA> model,
                             ElementId elementId, String style ) {
         super(false, elementId.child(CONTAINER_ID), style);
 
         this.swatchTexture = defaultTexture;
- 
-        SpringGridLayout layout = new SpringGridLayout();      
+
+        SpringGridLayout layout = new SpringGridLayout();
         getControl(GuiControl.class).setLayout(layout);
- 
+
         colorPanel = new Container(elementId.child(COLORS_ID), style);
-        colorPanel.setLayout(new SpringGridLayout());       
+        colorPanel.setLayout(new SpringGridLayout());
         colors = new Panel();
         colorPanel.addChild(colors);
         this.swatchComponent = new QuadBackgroundComponent(swatchTexture);
-        CursorEventControl.addListenersToSpatial(colors, new SwatchListener()); 
+        CursorEventControl.addListenersToSpatial(colors, new SwatchListener());
         colors.setBackground(swatchComponent);
         colors.setPreferredSize(new Vector3f(256, 64, 0));
         layout.addChild(colorPanel, 2);
@@ -132,20 +133,20 @@ public class ColorChooser extends Panel {
         brightness = new Slider(Axis.Y, elementId.child(BRIGHTNESS_ID), style);
         layout.addChild(brightness, 1);
         brightnessRef = brightness.getModel().createReference();
- 
+
         value = new Panel(elementId.child(VALUE_ID), style);
         value.setPreferredSize(new Vector3f(64, 64, 0));
         value.setBackground(valueColor);
-        layout.addChild(value, 0); 
-               
+        layout.addChild(value, 0);
+
         if( applyStyles ) {
             Styles styles = GuiGlobals.getInstance().getStyles();
             styles.applyStyles(this, getElementId(), style);
         }
 
-        setModel(model);        
+        setModel(model);
     }
- 
+
     public void setModel( VersionedObject<ColorRGBA> model ) {
         if( this.model != null ) {
             // clean up whatever
@@ -157,23 +158,23 @@ public class ColorChooser extends Panel {
         this.model = model;
         modelRef = model.createReference();
         updateColorView();
-    }    
-    
+    }
+
     public VersionedObject<ColorRGBA> getModel() {
         return model;
-    }       
- 
+    }
+
     @Override
     public void updateLogicalState( float tpf ) {
         super.updateLogicalState(tpf);
         if( modelRef.update() ) {
             updateColorView();
-        } 
+        }
         if( brightnessRef.update() ) {
             updateBrightness();
         }
     }
- 
+
     protected void updateModelValue( float h, float s, float b ) {
         if( h == hIndex && s == sIndex && b == bIndex ) {
             return;
@@ -181,30 +182,30 @@ public class ColorChooser extends Panel {
         this.hIndex = h;
         this.sIndex = s;
         this.bIndex = b;
-        
+
         Color awtColor = Color.getHSBColor(hIndex, sIndex, bIndex);
-        ((VersionedHolder<ColorRGBA>)model).setObject(toJmeColor(awtColor));                     
+        ((VersionedHolder<ColorRGBA>)model).setObject(toJmeColor(awtColor));
     }
- 
+
     protected void updateBrightness() {
         float v = (float)(brightness.getModel().getValue()/100);
         updateModelValue(hIndex, sIndex, v);
     }
- 
+
     protected static ColorRGBA toJmeColor( Color clr ) {
-        float r = clr.getRed() / 255f; 
-        float g = clr.getGreen() / 255f; 
+        float r = clr.getRed() / 255f;
+        float g = clr.getGreen() / 255f;
         float b = clr.getBlue() / 255f;
-        return new ColorRGBA(r, g, b, 1); 
+        return new ColorRGBA(r, g, b, 1);
     }
-    
+
     protected void updateColorView() {
- 
+
         ColorRGBA c = model.getObject();
-         
-        int r = (int)Math.round(c.getRed() * 255); 
-        int g = (int)Math.round(c.getGreen() * 255); 
-        int b = (int)Math.round(c.getBlue() * 255); 
+
+        int r = (int)Math.round(c.getRed() * 255);
+        int g = (int)Math.round(c.getGreen() * 255);
+        int b = (int)Math.round(c.getBlue() * 255);
         float[] hsb = Color.RGBtoHSB(r, g, b, null);
 
         this.hIndex = hsb[0];
@@ -213,27 +214,27 @@ public class ColorChooser extends Panel {
 
         updateColorView(hsb[0], hsb[1], hsb[2]);
     }
-    
+
     protected void updateColorView( float h, float s, float v ) {
- 
+
         Color awtColor = Color.getHSBColor(h, s, v);
-        valueColor.setColor(toJmeColor(awtColor));             
-            
+        valueColor.setColor(toJmeColor(awtColor));
+
         // Now we need to get the B of the HSB to set that one
-        brightness.getModel().setValue(v * 100);               
+        brightness.getModel().setValue(v * 100);
     }
-    
+
     private class SwatchListener extends DefaultCursorListener {
-    
+
         @Override
         protected void click( CursorButtonEvent event, Spatial target, Spatial capture ) {
-        
+
             Vector3f world = new Vector3f(event.getX(), event.getY(), 0);
             Vector3f local = colors.worldToLocal(world, null);
             Vector3f size = colors.getSize();
             float h = (local.x / size.x);
-            float s = (size.y + local.y) / size.y; 
-            updateModelValue(h, s, bIndex);                                   
+            float s = (size.y + local.y) / size.y;
+            updateModelValue(h, s, bIndex);
         }
-    }        
+    }
 }
