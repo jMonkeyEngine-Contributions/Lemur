d342c8e6bbbfa56114dafd49dd3f9a6f9c6ba509
Paul Speed <pspeed42@users.noreply.github.com>
2016-12-03 13:20:01 -0500

    Added a DocumentModelFilter base class that can also be used as is for basic filtering of input and output using Guava functions. Added a TextFilters utility factory class that provides a bunch of standard functions and predicates for commonly used filtering.

---
+++ src/main/java/com/simsilica/lemur/text/DocumentModelFilter.java
@@ -0,0 +1,318 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2016, Simsilica, LLC
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.simsilica.lemur.text;
+
+import java.util.Objects;
+
+import com.google.common.base.Function;
+
+import com.simsilica.lemur.core.VersionedReference;
+
+/**
+ *  A convenient base class for DocumentModel filtering that simply passes
+ *  most methods through to the wrapped delegate DocumentModel, while intercepting
+ *  others for subclasses to filter.  Subclasses can override the filterInput()
+ *  and filterOutput() methods to provide simpler filtering or simply set
+ *  Function implementations for those transforms.
+ *  Advanced output filtering (where the output string maybe a different
+ *  size then what is kept by the delegate) would require more extensive overriding
+ *  of behavior to track carat location, line, column, and so on.
+ *
+ *  @author    Paul Speed
+ */
+public class DocumentModelFilter implements DocumentModel {
+ 
+    private DocumentModel delegate;
+    private Function<Character, Character> inputTransform;
+    private Function<String, String> outputTransform;
+    private String lastOutput = null;
+    private String lastTransformedOutput = null;
+    private long version;
+    
+    public DocumentModelFilter() {
+        this(new DefaultDocumentModel());
+    }
+    
+    public DocumentModelFilter( DocumentModel delegate ) {
+        this.delegate = delegate;
+    } 
+
+    /**
+     *  Returns the delegate document model for which this document model
+     *  filter is filtering,
+     */
+    public DocumentModel getDelegate() {
+        return delegate;
+    }
+
+    /**
+     *  Sets an optional output transform function that will convert
+     *  text before returning it from getText().  Note: with the default
+     *  implementation of DocumentModelFilter, the transformed text must
+     *  be a character for character replacement or the carat position
+     *  will be incorrectly reported from the callers' perspective.
+     *  If the supplied function is null then no output transformation
+     *  is done by default.
+     */
+    public void setOutputTransform( Function<String, String> f ) {
+        if( outputTransform == f ) {
+            return;
+        }
+        this.outputTransform = f;
+        version++;
+    }
+    
+    public Function<String, String> getOutputTransform() {
+        return outputTransform;
+    }
+    
+    /**
+     *  Sets an optional input transform that will be used to convert
+     *  all input from setText() or insert().  If the function returns null
+     *  for any character then that character is skipped in the input.
+     *  If the supplied function is null then no input transformation is 
+     *  done by default.
+     */
+    public void setInputTransform( Function<Character, Character> f ) {
+        if( inputTransform == f ) {
+            return;
+        }
+        this.inputTransform = f;
+        version++;
+    }
+    
+    public Function<Character, Character> getInputTransform() {
+        return inputTransform;
+    } 
+    
+    public DocumentModel clone() {
+        return new DocumentModelFilter(delegate.clone());
+    }
+
+    /**
+     *  Can be overridden to bulk filter input provided to
+     *  setText().  Default implementation returns the text directly
+     *  unless there is an input transform function set.  If there is
+     *  an input transform function set then each character is first
+     *  passed through that function to build a new string.  
+     */
+    protected String filterInput( String text ) {
+        if( inputTransform != null ) {
+            StringBuilder sb = new StringBuilder();
+            for( int i = 0; i < text.length(); i++ ) {
+                char c = text.charAt(i);
+                Character x = inputTransform.apply(c);
+                if( x != null ) {
+                    sb.append(x);
+                }
+            }
+            return sb.toString();
+        }
+        return text;
+    }
+
+    /**
+     *  Can be overridden to filter the input provided to
+     *  insert().  The default implementation returns the character directly.
+     */
+    protected Character filterInput( char c ) {
+        if( inputTransform != null ) {
+            return inputTransform.apply(c);
+        }        
+        return c;
+    }
+
+    /**
+     *  Can be overridden to filter the output from the getText()
+     *  method, for example replacing all characters with '*' for
+     *  a password field.  The default implementation checks for
+     *  a transform function and uses it or just returns the string
+     *  directly. 
+     */
+    protected String filterOutput( String text ) {
+        if( outputTransform != null ) {
+            return outputTransform.apply(text);
+        }
+        return text;
+    }
+
+    @Override
+    public void setText( String text ) {
+        delegate.setText(filterInput(text));
+    }
+
+    /**
+     *  Passes the delegate's getText() through the local filterOutput()
+     *  method before returning.
+     */
+    @Override
+    public String getText() {
+        String output = delegate.getText();
+        if( Objects.equals(output, lastOutput) ) {
+            return lastTransformedOutput;
+        }
+        // Cache the results for next time.  If the delegate's text
+        // doesn't change then we avoid needlessly calling the transform.
+        lastOutput = output;
+        lastTransformedOutput = filterOutput(output);  
+        return lastTransformedOutput;  
+    } 
+
+    /**
+     *  Passes the delegate's getLine() through the local filterOutput()
+     *  method before returning.
+     */
+    @Override
+    public String getLine( int line ) {
+        return filterOutput(delegate.getLine(line));
+    } 
+
+    @Override
+    public int getLineCount() {
+        return delegate.getLineCount();
+    } 
+
+    @Override
+    public int getCarat() {
+        return delegate.getCarat();
+    } 
+
+    @Override
+    public int getCaratLine() {
+        return delegate.getCaratLine();
+    } 
+
+    @Override
+    public int getCaratColumn() {
+        return delegate.getCaratColumn();
+    } 
+
+    @Override
+    public int getAnchorLine() {
+        return delegate.getAnchorLine();
+    } 
+
+    @Override
+    public int getAnchorColumn() {
+        return delegate.getAnchorColumn();
+    }
+
+    @Override
+    public int getAnchor() {
+        return delegate.getAnchor();
+    } 
+
+    @Override
+    public int home( boolean currentLine ) {
+        return delegate.home(currentLine);
+    } 
+
+    @Override
+    public int end( boolean currentLine ) {
+        return delegate.end(currentLine);
+    } 
+    
+    @Override
+    public int up() {
+        return delegate.up();
+    } 
+ 
+    @Override
+    public int down() {
+        return delegate.down();
+    } 
+
+    @Override
+    public int left() {
+        return delegate.left();
+    } 
+
+    @Override
+    public int right() {
+        return delegate.right();
+    }
+
+    @Override
+    public void insertNewLine() {
+        delegate.insertNewLine();
+    } 
+
+    @Override
+    public void deleteCharAt( int pos ) {
+        delegate.deleteCharAt(pos);
+    } 
+
+    @Override
+    public void backspace() {
+        delegate.backspace();
+    } 
+
+    @Override
+    public void delete() {
+        delegate.delete();
+    } 
+
+    @Override
+    public void insert( char c ) {
+        Character x = filterInput(c);
+        if( x != null ) { 
+            delegate.insert(x);
+        }
+    } 
+
+    @Override
+    public long getVersion() {
+        // We include our local version in case we need to update views
+        // of ourselves even if the underlying model hasn't changed.
+        return delegate.getVersion() + version;
+    }
+    
+    @Override
+    public DocumentModel getObject() {
+        return this;
+    }
+    
+    @Override
+    public VersionedReference<DocumentModel> createReference() {
+        return new VersionedReference<DocumentModel>(this);
+    }
+    
+    @Override
+    public VersionedReference<Integer> createCaratReference() {
+        return delegate.createCaratReference();
+    }    
+}
---
+++ src/main/java/com/simsilica/lemur/text/TextFilters.java
@@ -0,0 +1,272 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2016, Simsilica, LLC
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.simsilica.lemur.text;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+
+/**
+ *  A collection of convenience filters for input and output.
+ *
+ *  @author    Paul Speed
+ */
+public class TextFilters {
+ 
+    private static IsDigit IS_DIGIT = new IsDigit();
+    private static IsLetter IS_LETTER = new IsLetter();
+    private static Function<Character, Character> ALPHA = charFilter(IS_LETTER);
+    private static Function<Character, Character> NUMERIC = charFilter(IS_DIGIT);
+    private static Function<Character, Character> ALPHA_NUMERIC = charFilter(Predicates.or(IS_DIGIT, IS_LETTER));
+    private static ToLowerCase TO_LOWER_CASE = new ToLowerCase(); 
+    private static ToUpperCase TO_UPPER_CASE = new ToUpperCase(); 
+    
+    /**
+     *  A text output transform that replaces all characters in the string (including
+     *  non-line feed whitespace) with the specified character.  This is useful
+     *  for fields that must obscure the real data like password fields. 
+     */
+    public static Function<String, String> constantTransform( char c ) {
+        return new ConstantOutputTransform(c);
+    }
+ 
+    /**
+     *  A text output transform that converts all characters to upper case in
+     *  the output string.
+     */
+    public static Function<String, String> upperCaseTransform() {
+        return new CharOutputTransform(TO_UPPER_CASE);
+    }
+ 
+    /**
+     *  A text output transform that converts all characters to lower case in
+     *  the output string.
+     */
+    public static Function<String, String> lowerCaseTransform() {
+        return new CharOutputTransform(TO_LOWER_CASE);
+    }
+    
+    /** 
+     *  A text output transform that passes all characters through a character
+     *  filter when passing them to the output string.  Note: any character
+     *  filters that skip characters will cause the standard DocumentModelFilter
+     *  carat position to be inaccruate... ie: the user will see their cursor
+     *  in the wrong place.  For standard DocumentModelFilter usage, make sure
+     *  the supplied filter always returns something for every character.
+     */
+    public static Function<String, String> charOutputTransform( Function<Character, Character> transform ) {
+        return new CharOutputTransform(transform);
+    }
+    
+    /**
+     *  A character filter that only allows numeric digits.
+     */
+    public static Function<Character, Character> alpha() {
+        return ALPHA;
+    }
+
+    /**
+     *  A character filter that only allows numeric digits.
+     */
+    public static Function<Character, Character> numeric() {
+        return NUMERIC;
+    }
+
+    /**
+     *  A character filter that only allows numeric digits.
+     */
+    public static Function<Character, Character> alphaNumberic() {
+        return ALPHA_NUMERIC;
+    }
+    
+    /**
+     *  A character filter that skips characters that do not pass the
+     *  specified predicate.
+     */
+    public static Function<Character, Character> charFilter( Predicate<Character> predicate ) {
+        return new CharFilter(predicate);
+    }
+ 
+    /**
+     *  A character filter that converts all passed characters to upper case
+     *  using Character.toUpperCase().
+     */
+    public static Function<Character, Character> toUpperCase() {
+        return TO_UPPER_CASE;
+    }
+
+    /**
+     *  A character filter that converts all passed characters to upper case
+     *  using Character.toUpperCase().
+     */
+    public static Function<Character, Character> toLowerCase() {
+        return TO_LOWER_CASE;
+    }
+    
+    /**
+     *  Returns a predicate that returns true for letter characters as
+     *  is passed to Character.isLetter().
+     */
+    public static Predicate<Character> isLetter() {
+        return IS_LETTER;
+    }
+
+    /**
+     *  Returns a predicate that returns true for numeric digit characters as
+     *  is passed to Character.isDigit().
+     */
+    public static Predicate<Character> isDigit() {
+        return IS_DIGIT;
+    }
+    
+    /**
+     *  Returns a predicate that returns true for any character in the specified
+     *  list of characters.
+     */
+    public static Predicate<Character> isInChars( char... chars ) {
+        return new IsInChars(chars);
+    }
+    
+    private static class ConstantOutputTransform implements Function<String, String> {
+        
+        private char output;
+        
+        public ConstantOutputTransform( char output ) {
+            this.output = output;
+        }
+        
+        public String apply( String input ) {
+            if( input == null ) {
+                return null;
+            }
+            if( input.length() == 0 ) {
+                return input;
+            }
+            StringBuilder result = new StringBuilder();
+            for( int i = 0; i < input.length(); i++ ) {
+                char c = input.charAt(i);
+                if( c == '\r' || c == '\n' ) {
+                    result.append(c);
+                } else {
+                    result.append(output);
+                }
+            }
+            return result.toString(); 
+        } 
+    }
+
+    private static class CharOutputTransform implements Function<String, String> {
+        
+        private Function<Character, Character> transform;
+        
+        public CharOutputTransform( Function<Character, Character> transform ) {
+            this.transform = transform;
+        }
+        
+        public String apply( String input ) {
+            if( input == null ) {
+                return null;
+            }
+            if( input.length() == 0 ) {
+                return input;
+            }
+            StringBuilder result = new StringBuilder();
+            for( int i = 0; i < input.length(); i++ ) {
+                Character c = input.charAt(i);
+                if( c != null ) {
+                    result.append(transform.apply(c));
+                }
+            }
+            return result.toString(); 
+        } 
+    }
+    
+    private static class CharFilter implements Function<Character, Character> {
+        
+        private Predicate<Character> predicate;
+    
+        public CharFilter( Predicate<Character> predicate ) {
+            this.predicate = predicate;
+        }
+        
+        public Character apply( Character c ) {
+            return predicate.apply(c) ? c : null;
+        } 
+    }
+ 
+    private static class ToUpperCase implements Function<Character, Character> {
+        public Character apply( Character c ) {
+            return Character.toUpperCase(c);
+        }       
+    }
+     
+    private static class ToLowerCase implements Function<Character, Character> {
+        public Character apply( Character c ) {
+            return Character.toLowerCase(c);
+        }
+    } 
+    
+    private static class IsDigit implements Predicate<Character> {
+        public boolean apply( Character c ) {
+            return Character.isDigit(c);
+        }
+    }
+       
+    private static class IsLetter implements Predicate<Character> {
+        public boolean apply( Character c ) {
+            return Character.isLetter(c);
+        }
+    }
+    
+    private static class IsInChars implements Predicate<Character> {
+        private char[] chars;
+        
+        public IsInChars( char[] chars ) {
+            this.chars = chars;
+        }
+    
+        public boolean apply( Character c ) {
+            for( char check : chars ) {
+                if( c.charValue() == check ) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }   
+}
